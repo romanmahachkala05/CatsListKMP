3b1f09bfa1789f2726cf92e5075f0d842ab3d9f5
user1234 <romansakhwork@gmail.com>
2026-09-23 12:59:42 +0300

    [TECH] Add Compose Multiplatform and a catslist.kmp.compose convention plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.androidx.room) apply false
     alias(libs.plugins.ktlint) apply false
